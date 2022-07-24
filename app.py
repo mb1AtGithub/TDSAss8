535117403f9a77b3d06a467fe5176a79ca186eaa
mb1AtGithub <96431770+mb1AtGithub@users.noreply.github.com>
2022-07-24 20:49:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import streamlit as st
 
+
+
+
 st.write("""
 # This app will check whether the number given by user is even or odd
 """)
 #Get Input
 
-st.header('Enter your number')
+st.title('Odd-Even Finder')
 
-num  = st.number_input("Number")
+num  = st.number_input("Enter a Number")
 
 op = ""
 
 if num ==0 :
   op= "your number is even."
 if num%2 == 1:
-  op= "your number is even."
+  op= "your number is odd."
 else:
-  op= "your number is odd." 
+  op= "your number is even." 
 
 st.write(op)
